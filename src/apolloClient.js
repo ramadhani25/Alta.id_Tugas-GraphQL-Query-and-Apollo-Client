2c420126b126c49fd7e851dbd57d98570ca94817
ramadhani25 <ramadhaniak1125@gmail.com>
2022-04-22 14:50:34 +0700

    fix admin secret

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -3,8 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://assured-ewe-89.hasura.app/v1/graphql",
   headers: {
-    "x-hasura-admin-secret":
-      "d9tYqpkkgmNGSicdr54rbbuSBunKo6xylQ7JTz0PqlDes5Mh3C36a6bpGMqwpubM",
+    "x-hasura-admin-secret": "iniadminsecret",
   },
   cache: new InMemoryCache(),
 });
